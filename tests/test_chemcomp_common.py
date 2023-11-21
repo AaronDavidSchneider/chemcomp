582260564b6f1e956f4f320a1df40a1651465cad
Aaron David Schneider <aaron.schneider@nbi.ku.dk>
2023-11-21 15:05:01 +0100

    increase threshold for disk

--- tests/test_chemcomp_common.py
+++ tests/test_chemcomp_common.py
@@ -5,8 +5,8 @@ import pytest
 
 # mask out all the data at which we have infinitesimally small values
 # -> numerical noise may be important there
-THRESHOLD_DISK = 1e-60
-MAX_RTOL_DISK = 1e-4
+THRESHOLD_DISK = 1e-30
+MAX_RTOL_DISK = 1e-3
 MAX_RTOL_PLANET = 1e-4
 
 reference = {
